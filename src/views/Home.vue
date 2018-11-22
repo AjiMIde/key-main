eeb72bb45bd4f365f4b8942623e1899126ce54d8
AjiMIde <chenjitao@phone580.com>
2018-11-22 17:01:01 +0800

    Commit the dev file and docs

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <!--<img alt="Vue logo" src="../assets/logo.png">-->
+    <HelloWorld v-if="false" msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-col :span="12">
+        <h4>{{this.nav.title}}</h4>
+        <ul>
+          <li v-for="(item, index) in this.nav.list" :key="index">
+            <a :href="item.link" target="_blank"> {{item.text}} </a>
+          </li>
+        </ul>
+      </el-col>
+      <el-col :span="12">xx</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import {Row, Col} from 'element-ui'
+import Nav from '@/datas/nav.json'
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      nav: Nav
+    }
+  },
+  components: {
+    'el-row': Row,
+    'el-col': Col,
+    HelloWorld
+  }
+}
+</script>
