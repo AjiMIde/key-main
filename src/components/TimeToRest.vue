d3cdca0a233bea33042d26dce9be873a01a048ae
AjiMIde <chenjitao@phone580.com>
2019-01-06 21:57:43 +0800

    Add chrome notification

--- src/components/TimeToRest.vue
+++ src/components/TimeToRest.vue
@@ -1,34 +1,76 @@
 <template>
   <div class="time-to-rest">
     <div class="time-clock">{{time}}</div>
+    <el-btn type="primary" size="small" @click="setNotify('TEST', 'TEST', '')">测试Chrome Notification</el-btn>
   </div>
 </template>
 
 <script>
+import { Button } from 'element-ui'
+import NTF from '../libs/ChromeNTF'
+
 export default {
   name: 'TimeToRest',
+  components: {
+    'el-btn': Button
+  },
   data () {
     return {
-      time: ''
+      time: '',
+      clogStyle: 'font-size: 14px; color: blue;font-family: Microsoft YaHei;',
+      matterTip: {
+        notification: null,
+        iKnown: false
+      }
     }
   },
   methods: {
     addZero (str) {
       str = '' + str
-      if (str.length >= 2) {
-        return str
-      } else {
-        return '0' + str
+      return (str.length >= 2 ? str : '0' + str)
+    },
+    startTimer () {
+      window.setInterval(() => {
+        const date = new Date()
+        const h = this.addZero(date.getHours())
+        const m = this.addZero(date.getMinutes())
+        const s = this.addZero(date.getSeconds())
+        this.time = h + ':' + m + ':' + s
+
+        if (m === '30' && s === '00') {
+          this.setNotify('TIME', 'WALK')
+          this.matterTip.iKnown = true
+        }
+        if (m === '30' && s === '10' && !this.matterTip.iKnown) {
+          this.matterTip.iKnown = true
+        }
+        if (m === '00' && s === '00') {
+          this.matterTip.iKnown = true
+          this.setNotify('TIME', 'WATER')
+        }
+        if (m === '00' && s === '10' && !this.matterTip.iKnown) {
+          window.alert('TIME' + 'WATER')
+        }
+      }, 1000)
+    },
+    setNotify (title = '', body = '', icon = '') {
+      if (title === '' || body === '') {
+        title = 'HELLO'
+        body = 'READY TO WORK?'
+      }
+
+      this.matterTip.notification = NTF.show(title, body, icon)
+      this.matterTip.iKnown = false
+
+      this.matterTip.notification.onclick = () => {
+        this.matterTip.notification.close()
+        this.matterTip.iKnown = true
       }
     }
   },
   mounted () {
-    window.setInterval(() => {
-      const date = new Date()
-      this.time = this.addZero(date.getHours()) + ':' + this.addZero(date.getMinutes()) +
-        ':' + this.addZero(date.getSeconds())
-
-    }, 1000)
+    this.startTimer()
+    this.setNotify()
   }
 }
 </script>
---
+++ src/libs/ChromeNTF.js
@@ -0,0 +1,38 @@
+const NTF = 'Notification'
+const ntf = window.Notification
+
+export default {
+  support: NTF in window,
+  permission: ntf.permission,
+  canIUse: ntf.permission === 'granted',
+  request (callback) {
+    ntf.requestPermission((permission) => {
+      if (callback && typeof callback === 'function') {
+        const bool = (permission === 'granted')
+        callback(bool)
+      }
+    })
+  },
+  show (title = '', body = '', icon = '') {
+    if (this.canIUse) {
+      return new window.Notification(title, {
+        body: body,
+        tag: 1,
+        icon: icon
+      })
+    } else {
+      this.request(canIUse => {
+        if (canIUse) {
+          return new window.Notification(title, {
+            body: body,
+            tag: 1,
+            icon: icon
+          })
+        } else {
+          window.alert('请允许使用桌面通知~~')
+          return null
+        }
+      })
+    }
+  }
+}
